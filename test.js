f2185af312dfdeab193810876f18d4f4c4d9171f
Viesturs Lujans <viesturs.lujans@gmail.com>
2017-01-13 13:49:52 +0200

    Update test.js

--- test.js
+++ test.js
@@ -32,35 +32,7 @@ console.log(345);
                 currencySelectId: '#shipping-currency'
             },
             _create: function() {
-                var self          = this,
-                    cacheAutoOpen = this.options.cacheAutoOpen;
-console.log(123);
-                // sets value to Cache auto open
-                this.options.popup.autoOpen = cacheAutoOpen;
-                if (!$.mage.cookies.get('store') || this.options.isCheckout) {
-                    if (this.options.isCheckout || this.options.isHeader) {
-                        this.options.popup.modalCloseBtn = '[data-role="closeBtn"]';
-                    }
-                    var popup = modal(this.options.popup, this.element);
-
-                    // Disable Escape Key
-                    if (!this.options.isCheckout && !this.options.isHeader) {
-                        popup.keyEventHandlers.escapeKey = function () {
-                            return;
-                        };
-                    }
-
-                    if (this.options.actionButtonId) {
-                        $(document).on('click', this.options.actionButtonId, function () {
-                            self.element.modal('openModal');
-                        });
-                    }
-                }
-
-                self.options.submitButton.on('click', function(e) {
-                    var selectedCode = $(self.options.currencySelectId).find(':selected').attr('data-storecode');
-                    self.options.storeCodeInput.val(selectedCode);
-                });
+                console.log(11);
             }
         });
 
